1105b43535d137ee539c7be8a828bd3f0f540f37
quyet1702 <quyet1702@gmail.com>
2021-05-01 22:51:26 +0700

    update clock

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// BEGIN: HEADER__CLOCK
 function date() {
         var date = new Date();
         var hours = date.getHours();
@@ -6,9 +7,11 @@ function date() {
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
         var day = date.getDate();
-        return `${hours}:${minutes}:${seconds}  ${day}/${month}/${year}`
+        return `${hours}:${minutes}:${seconds} -- ${day}/${month}/${year}`
 }
-setInterval(function(){
-    console.log(date());
-}, 1000);
 
+var clock = document.getElementById('header__clock');
+setInterval(function(){
+    clock.textContent = date();
+}, 1000);
+// END: HEADER__CLOCK
